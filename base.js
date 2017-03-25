cf5ded9161538439bd9baa656f270a0c73f47d06
dat13ili <dat13ili@studet.lu.se>
2017-03-25 10:08:22 +0100

    text changes

--- base.js
+++ base.js
@@ -1,3 +1,14 @@
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
 function getCookie(key) {
 	var value = "; " + document.cookie;
 	var parts = value.split("; " + key + "=");
@@ -10,7 +21,8 @@ function diffie(gen,secret) {
 }
 
 function stringToIntKey(str) {
-	return parseInt(str.split("").map((x)=>x.charCodeAt(0)).reduce((a,b)=>""+a+b));
+	return Math.abs(str.hashCode());
+	// return parseInt(str.split("").map((x)=>x.charCodeAt(0)%10).reduce((a,b)=>""+a+b,0));
 }
 
 var cookieSecret = getCookie("secret");
