d198f60750f56d11ed77a8667f666b5386627747
dat13ili <dat13ili@studet.lu.se>
2017-03-25 03:16:12 +0100

    support för text lösenord

--- base.js
+++ base.js
@@ -9,6 +9,10 @@ function diffie(gen,secret) {
 	return Math.abs((gen^secret) % 9321478927);
 }
 
+function stringToIntKey(str) {
+	return parseInt(str.split("").map((x)=>x.charCodeAt(0)).reduce((a,b)=>""+a+b));
+}
+
 var cookieSecret = getCookie("secret");
 if (cookieSecret !== null) {
 	document.getElementById("secret").value = cookieSecret;
